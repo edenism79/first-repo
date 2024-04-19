dc1849809c0267ef8e13458b3eea273a5a522d89
edenism79 <edenism79@gmail.com>
2024-04-19 15:20:55 +0900

    Style: Add Whitespace to split_odd_even

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def check_available():
     return 'It works!'
 
-#def remove_odd(nums=[1,2,3,4]):
+#def remove_odd(nums = [1,2,3,4]):
 #    return [num for num in nums if num%2==0]
 def split_odd_even(nums=[1,2,3,4]):
     odds = []
